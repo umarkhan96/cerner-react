9f5b0b94a972d7c2f53c81818614e058452b39cb
umarkhan96 <umarkhan_96@outlook.com>
2020-04-16 10:58:55 +0100

    commit

--- client/src/pages/launcher/launcher.js
+++ client/src/pages/launcher/launcher.js
@@ -8,7 +8,7 @@ export default class Launcher extends Component{
             clientId:"d3ea1926-228b-4d96-8e86-f1b419aa1cb0",
             scope:'patient/Patient.read patient/Observation.read patient/AllergyIntolerance.read patient/Appointment.read launch/patient online_access openid profile',
             redirectUri:"/data",
-            iss:"https://fhir-myrecord.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1cas",
+            iss:"https://fhir-myrecord.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca",
             completeInTarget:true
         })
     }
